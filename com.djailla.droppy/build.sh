026d3b7bbffdb075897e9d8562ec6b8dac2a8a0d
Djailla <bastien.vallet@gmail.com>
2016-05-27 16:03:07 +0200

    Cleanup build script
    Fix bad indent
    Remove curl and cache info

--- com.djailla.droppy/build.sh
+++ com.djailla.droppy/build.sh
@@ -11,15 +11,18 @@ apt-get update
 apt-get install -y -q curl
 if [ $RAINBOW_ARCHITECTURE = "x86_64" ]
 then
-    # Install nodejs components
     curl -sL https://deb.nodesource.com/setup_5.x | bash -
     apt-get install -y -q nodejs npm
 else
-	curl -O  http://node-arm.herokuapp.com/node_latest_armhf.deb
-	dpkg -i node_latest_armhf.deb
+    curl -O  http://node-arm.herokuapp.com/node_latest_armhf.deb
+    dpkg -i node_latest_armhf.deb
 fi
 
-# # Install the component
+# Install the component
 npm install -g droppy
+
+# Cleanup
+apt-get autoremove curl
+apt-get clean
 
 exit 0
