97ccfa89012e2b30623dd7a8651166c4ad98898c
Djailla <bastien.vallet@gmail.com>
2016-12-28 23:54:05 +0100

    Improve arch detection and bump to NodeJS 7

--- com.djailla.droppy/build.sh
+++ com.djailla.droppy/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $RAINBOW_ARCHITECTURE != "x86_64" ]
+then
+    echo "No ARM compatible version"
+    exit 1
+fi
+
 mkdir -m 755 -p /opt/droppy/
 
 install -m 755 /home/source/rc.local /etc
@@ -9,16 +15,11 @@ install -m 755 /home/source/config.json /opt/droppy
 # Install nodejs components
 apt-get update
 apt-get install -y -q curl
-if [ $RAINBOW_ARCHITECTURE = "x86_64" ]
-then
-    curl -sL https://deb.nodesource.com/setup_6.x | bash -
-    apt-get install -y -q nodejs
-else
-    echo "No ARM compatible version"
-    exit 1
-fi
+curl -sL https://deb.nodesource.com/setup_7.x | bash -
+apt-get install -y -q nodejs
 
 # Install the component
+npm config set unsafe-perm true
 npm install -g droppy
 
 # Cleanup
